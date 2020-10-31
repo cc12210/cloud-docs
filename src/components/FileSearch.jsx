94d1def8241f13945ab93c45400a0ebb45c4894d
caocheng <caocheng@tencent.com>
2020-10-31 17:19:14 +0800

    feat: 添加图标库

--- src/components/FileSearch.jsx
+++ src/components/FileSearch.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faTimesCircle } from "@fortawesome/free-solid-svg-icons";
 
 const FileSearch = ({ title, onFileSearch }) => {
   const [inputActive, setInputActive] = useState(false);
   const [keyWords, setKeyWords] = useState("");
 
+  // useRef可以记录节点内容等
   let node = useRef(null);
 
   // 清空值及关闭输入框
@@ -31,11 +34,11 @@ const FileSearch = ({ title, onFileSearch }) => {
     };
   });
 
-  useEffect((e) => {
-    if (inputActive) {
-      console.log(node.current);
-    }
-  });
+  // useEffect((e) => {
+  //   if (inputActive) {
+  //     console.log(node.current);
+  //   }
+  // });
   return (
     <div className="alert alert-primary mb-0">
       {/* 标题提示 */}
@@ -49,33 +52,33 @@ const FileSearch = ({ title, onFileSearch }) => {
               setInputActive(true);
             }}
           >
-            搜索
+            <FontAwesomeIcon title="搜索" icon={faSearch} />
           </button>
         </div>
       )}
       {/* 搜索框输入 */}
       {inputActive && (
-        <div className="row">
+        <div className="d-flex justify-content-between align-items-center">
           <input
             ref={node}
             autoFocus
-            className="form-control col-8"
+            className="form-control"
             value={keyWords}
             onChange={(e) => {
               setKeyWords(e.target.value);
             }}
-            onBlur={() => {
-              setInputActive(false);
-            }}
+            // onBlur={() => {
+            //   setInputActive(false);
+            // }}
           ></input>
           <button
             type="button"
-            className="btn btn-primary col-4"
-            onClick={() => {
-              setInputActive(true);
+            className="btn btn-primary"
+            onClick={(e) => {
+              closeSearch(e);
             }}
           >
-            关闭
+            <FontAwesomeIcon icon={faTimesCircle} />
           </button>
         </div>
       )}
